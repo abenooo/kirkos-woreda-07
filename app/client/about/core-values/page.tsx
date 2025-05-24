dda8017079e9a0db7fab6fe77203475973ae3397
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-05-24 04:07:27 -0700

    Refactor CoreValuesPage component to remove unused icon import and adjust core values text for improved clarity and layout.

--- app/client/about/core-values/page.tsx
+++ app/client/about/core-values/page.tsx
@@ -1,4 +1,4 @@
-import { AlertTriangle, Award } from "lucide-react";
+import { Award } from "lucide-react";
 
 export default function CoreValuesPage() {
   return (
@@ -6,10 +6,9 @@ export default function CoreValuesPage() {
       <Award size={48} className="h-12 w-12 text-[#1E90FF] mb-6" />
       <h1 className="text-2xl text-primary font-bold uppercase mb-4">መርሆ</h1>
       <p className="text-gray-700 max-w-2xl">
-        ግልፀኝነትና: ተጠያቂነት : ቅንነት: ታማኝነት: ተነሳሽነት: የላቀ አገልግሎት : የህግ የበላይነት: የላቀ የሕዝብ ጥቅም :
-        በእውቀትና በእምነት መምራት
+        ግልፀኝነትና: ተጠያቂነት : ቅንነት: ታማኝነት: ተነሳሽነት: የላቀ አገልግሎት : የህግ የበላይነት: የላቀ የሕዝብ
+        ጥቅም : በእውቀትና በእምነት መምራት
       </p>
-      {/* Add actual core values content here, replacing the paragraph */}
     </div>
   );
 }
