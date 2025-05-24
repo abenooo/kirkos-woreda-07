558cc9f8c07b11089506b5614f3059937b7ec3c4
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-05-24 04:49:48 -0700

    Refactor sitemap.xml/route.ts to improve sitemap generation by updating static paths and utilizing NextRequest for better request handling.

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,40 +1,41 @@
 // app/sitemap.xml/route.ts
-export async function GET() {
-    const BASE_URL = "https://www.kirkosw07pubservice.org/sitemap.xml"
-  
-    const staticPages = [
-      '',
-      'services',
-      'news',
-      'requirements',
-      'search',
-      'complaints',
-      'anonymous',
-      'about',
-      'about/core-values',
-      'about/mission',
-      'about/vision',
-      'sitemap',
-    ];
-  
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPages
-        .map((path) => {
-          return `
-        <url>
-          <loc>${`${BASE_URL}/${path}`}</loc>
-          <changefreq>monthly</changefreq>
-          <priority>0.8</priority>
-        </url>`;
-        })
-        .join('')}
-    </urlset>`;
-  
-    return new Response(sitemap, {
-      headers: {
-        'Content-Type': 'application/xml',
-      },
-    });
-  }
-  
+
+import { NextRequest } from 'next/server';
+
+const BASE_URL = 'https://www.kirkosw07pubservice.org';
+
+export async function GET(_req: NextRequest) {
+  const staticPaths = [
+    '/',
+    '/services',
+    '/news',
+    '/requirements',
+    '/search',
+    '/complaints',
+    '/anonymous',
+    '/about',
+    '/about/core-values',
+    '/about/mission',
+    '/about/vision',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${staticPaths
+  .map(
+    (path) => `
+  <url>
+    <loc>${`${BASE_URL}${path}`}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join('')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
